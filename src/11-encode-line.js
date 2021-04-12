6cb7c68abb6abab70d2048f3d77745fb1ecb648c
vlad23349 <78217932+vlad23349@users.noreply.github.com>
2021-04-13 01:35:14 +0300

    feat: update task

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,18 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let newStr = '';
+  let count = 1;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) count++;
+    else {
+      if (count === 1) newStr += str[i];
+      else newStr += count + str[i];
+      count = 1;
+    }
+  }
+  return newStr;
 }
 
 module.exports = encodeLine;
